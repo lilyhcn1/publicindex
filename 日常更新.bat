990c27ee609d0c5d0e47b0763a8f39f477758398
lilyhcn1 <lilyhcn1@upsir.com>
2021-06-06 21:36:12 +0800

    注释语句1

--- 日常更新.bat
+++
@@ -1,8 +0,0 @@
-git add .
-git commit -m "×¢ÊÍÓï¾ä"
-git remote add origin git@github.com:lilyhcn1/publicindex.git
-git pull origin master
-git push -u origin master
-
-start https://lilyhcn1.github.io/publicindex/
-pause
